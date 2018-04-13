1b0a2cbd8ebe5bd42ef83c0d39447a810a0f03c9
tonyonawole@gmail.com <tonyonawole@gmail.com>
2018-04-13 15:36:58 +0100

    * .gitignore: * square.sln: * Main.cs: * Info.plist: * App.xaml: * AppDelegate.cs: * packages.config: * square.shproj: * Item.cs: * square.iOS.csproj: * packages.config: * MainActivity.cs: * Entitlements.plist: * square.projitems: * MainPage.cs: * square.Droid.csproj: * tab_feed.png: * Program.cs: * tab_about.png: * LaunchScreen.storyboard: * ItemsPage.xaml: * Startup.cs: * AboutPage.xaml: * AboutAssets.txt: * IDataStore.cs: * tab_feed@2x.png: * tab_feed@3x.png: * NewItemPage.xaml: * tab_about@2x.png: * tab_about@3x.png: * AboutPage.xaml.cs: * xamarin_logo.png: * ItemsPage.xaml.cs: * Item.cs: * MockDataStore.cs: * ItemDetailPage.xaml: * NewItemPage.xaml.cs: * AssemblyInfo.cs: * xamarin_logo@2x.png: * xamarin_logo@3x.png: * Contents.json: * icon.png: * styles.xml: * CloudDataStore.cs: * appsettings.json: * BaseViewModel.cs: * AboutResources.txt: * Tabbar.axml: * ItemDetailPage.xaml.cs: * AboutViewModel.cs: * ItemsViewModel.cs: * Toolbar.axml: * Resource.designer.cs: * AndroidManifest.xml: * icon.png: * icon.png: * ItemDetailViewModel.cs: * icon.png: * xamarin_logo.png: * ItemRepository.cs: * IItemRepository.cs: * appsettings.Development.json: * ItemController.cs: * square.MobileAppService.csproj: * launchSettings.json: * Contents.json:
    * App.xaml.cs: V1.0

---
+++ square/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace square
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
---
+++ square/Views/AboutPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace square
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
